49827e2b8eff59441588087f44b30456d4162696
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2021-12-07 21:30:15 +0600

    complexObject destructure

--- object.js
+++ object.js
@@ -5,3 +5,12 @@ const bim ={name:"Ertugrul bey",age:45,address:"Hanli Pazar", gfName:"Halima Hat
 const {fname,address,gfName,child}=bim
 console.log(fname,address);
 console.log(fname,address,gfName,child);
+const complexObject = {
+    name:"abc",
+    info:{
+        address:"karashahigar",
+        leader:"Ural Bey"
+    }
+}
+const {leader}=complexObject.info
+console.log(leader);
