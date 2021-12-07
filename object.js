ef7b6f67b79574d4f5a8312ba0797e92acbae77c
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2021-12-07 21:18:00 +0600

    object destructure

---
+++ object.js
@@ -0,0 +1,7 @@
+const bim ={name:"Ertugrul bey",age:45,address:"Hanli Pazar", gfName:"Halima Hatun", fname:"Sulayman Shah", child:3, obasi:"kayi",pet:"aktul Gali"}
+// const fname = bim.fname;
+// const address = bim.address;
+//destructure
+const {fname,address,gfName,child}=bim
+console.log(fname,address);
+console.log(fname,address,gfName,child);
