3342a9461c5c54d30da450e5b3ca5b6c7a8d2ac3
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-03-20 02:20:36 +0600

    function access from model class

---
+++ class-method.js
@@ -0,0 +1,22 @@
+class Student {
+    constructor(name, age) {
+
+        this.name = name
+        this.age = age
+        this.school = "IDT High School"
+    }
+    GetAge(){
+if(this.age){
+    console.log(this.age);
+}
+else
+return "no age"
+
+    }
+
+}
+const st2= new Student('karim')
+console.log(st2.GetAge());
+const student1 = new Student("Rahim",42)
+console.log(student1)
+
