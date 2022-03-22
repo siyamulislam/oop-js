03a27079ac125559fe87a4b49e985353aa8996f0
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-03-22 09:45:20 +0600

    formation changed

--- inheritance.js
+++ inheritance.js
@@ -1,22 +1,21 @@
-class Father{
-    constructor(){
-        this.fatherName="Eartugrul Gazi"
-    
-    }
+class Father {
+  constructor() {
+    this.fatherName = "Eartugrul Gazi";
+  }
 }
 class Child extends Father {
-    constructor(name,age){
-        super()
-        this.name=name
-        this.age=age
-    }
-    getFullName(){
-        return this.fatherName+ " olu "+this.name 
-    }
+  constructor(name, age) {
+    super();
+    this.name = name;
+    this.age = age;
+  }
+  getFullName() {
+    return this.fatherName + " olu " + this.name;
+  }
 }
 
-const bim=new Child("Osman Bey",25)
-const bim2=new Child("Gunduz Alp",32)
+const bim = new Child("Osman Bey", 25);
+const bim2 = new Child("Gunduz Alp", 32);
 console.log(bim);
 console.log(bim2);
 console.log(bim.getFullName());
